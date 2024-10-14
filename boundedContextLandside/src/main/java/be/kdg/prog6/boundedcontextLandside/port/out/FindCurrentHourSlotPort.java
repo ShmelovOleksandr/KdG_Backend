6a66558c899330a175a0c0d61ebc8244fab7bce7
Oleksandr Shmelov <oleksandr.shmelov@student.kd.be>
2024-10-14 16:08:17 +0200

    Implemented management of EntranceRequests

---
+++ boundedContextLandside/src/main/java/be/kdg/prog6/boundedcontextLandside/port/out/FindCurrentHourSlotPort.java
@@ -0,0 +1,7 @@
+package be.kdg.prog6.boundedcontextLandside.port.out;
+
+import be.kdg.prog6.boundedcontextLandside.domain.HourSlot;
+
+public interface FindCurrentHourSlotPort {
+    HourSlot findCurrentHourSlot();
+}
