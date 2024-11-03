79b23c2cc1db24a2332194a9357dfcdb58aa87f3
Oleksandr Shmelov <oleksandr.shmelov@student.kd.be>
2024-11-03 16:10:20 +0100

    Final fixes; Updated README

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# TemplateProject
-
-### TODO:
-
-Cant fetch warehouse activities
-
-Can create multiple PDT for the same Appointment (Possibly make appointment_id a PK)
-
-
-### Questions:
+# Krystal Distribution Group (backend)
--- boundedContextWaterside/src/main/java/be/kdg/prog6/boundedcontextWaterside/adapter/in/po/PORestController.java
+++ boundedContextWaterside/src/main/java/be/kdg/prog6/boundedcontextWaterside/adapter/in/po/PORestController.java
@@ -4,7 +4,6 @@ import be.kdg.prog6.boundedcontextWaterside.adapter.in.po.dto.POGetDto;
 import be.kdg.prog6.boundedcontextWaterside.domain.PO;
 import be.kdg.prog6.boundedcontextWaterside.port.in.GetAllFulfilledPOsUseCase;
 import be.kdg.prog6.boundedcontextWaterside.port.in.GetAllOutstandingPOsUseCase;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,10 +26,6 @@ public class PORestController {
     @GetMapping
     public ResponseEntity<List<POGetDto>> getPOs(@RequestParam(required = false) Boolean outstanding, @RequestParam(required = false) Boolean fulfilled) {
         List<PO> pos;
-//        if (outstanding)
-//            pos = getAllOutstandingPOsUseCase.getAllOutstandingPOs();
-//        else if (fulfilled)
-//            pos = getAllFulfilledPOsUseCase.getAllFulfilledPOs();
         if(outstanding == null && fulfilled != null && fulfilled)
             pos = getAllFulfilledPOsUseCase.getAllFulfilledPOs();
         else if (outstanding != null && fulfilled == null && outstanding)
